9d9fc735fe82706a7a4c8d82067af2cb6b3a20ac
tang-junjie <junjie.tang@hotmail.com>
2020-09-02 22:58:58 -0700

    minor tweak

--- settings/setup-js-mode.el
+++ settings/setup-js-mode.el
@@ -16,10 +16,8 @@
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
-  (company-mode +1))
-
-(setq company-tooltip-align-annotations t)
-
+  (company-mode +1)
+  (setq company-tooltip-align-annotations t))
 (add-hook 'js-mode-hook #'setup-tide-mode)
 
 ;; fix current file using ESLint --fix
